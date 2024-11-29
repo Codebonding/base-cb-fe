da95cb85ae967d7a7dc52f9872b7a8c77c09adaa
Codebonding <codebonding@gmail.com>
2024-11-29 21:58:09 +0530

    update

--- src/features/api/userApiSlice.js
+++ src/features/api/userApiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const userApiSlice = createApi({
     reducerPath: 'userApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://codebondingworkforce.com/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://codebondingworkforce.com/' }),
     endpoints: (builder) => ({
       createUser: builder.mutation({
         query: (userData) => {
