a50210cbf0a6d74f2cd9e3c5932bf76c13361c3b
codebonding <codebonding@gmail.com>
2025-03-17 15:41:27 +0530

    fc

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -146,12 +146,12 @@ const Header = () => {
               </ul>
             </nav>
             <div className="hidden md:flex space-x-4">
-              <button
+              {/* <button
                 onClick={() => navigate("/Candiate")}
                 className="px-6 py-2 text-white bg-[#105E6B] rounded-lg hover:bg-[#0C4A5F] transition-all duration-300 ease-in-out transform hover:scale-105 shadow-md hover:shadow-lg"
               >
                 Sign Up
-              </button>
+              </button> */}
               <button
                 onClick={() => navigate("/login")}
                 className="px-6 py-2 text-[#105E6B] bg-transparent border-2 border-[#105E6B] rounded-lg hover:bg-[#105E6B] hover:text-white transition-all duration-300 ease-in-out transform hover:scale-105 shadow-md hover:shadow-lg"
